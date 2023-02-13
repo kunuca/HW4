9c8daa2d08c3d2425a9187be9910f6cbcdfb3cbe
Tatiana Kustova <kunuca@mail.ru>
2023-02-13 14:53:32 +0400

    004

---
+++ 004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] numbers = new int[15];
+int sum=0;
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]= new Random().Next(0,100);
+    Console.Write($" {numbers[i]} \t");
+}
+for (int i = 1; i < numbers.Length; i=i+2) sum=sum+numbers[i];
+
+Console.WriteLine();
+Console.Write($"Сумма чисел массива с нечетным индексом равна {sum}");
