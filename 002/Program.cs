88394928ce10d92da5f1dbfe37b0bdcc6e0295ae
Tatiana Kustova <kunuca@mail.ru>
2023-02-13 14:37:54 +0400

    002

---
+++ 002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+Console.WriteLine("Hello, World!");
+
+int[] numbers = new int[15];
+int possitive=0;
+int negative=0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]= new Random().Next(0,1000);
+    Console.Write($" {numbers[i]} \t");
+}
+for (int i = 0; i < numbers.Length; i++)
+{ if (numbers[i]%2==0) possitive++;
+  else negative++;
+}
+Console.WriteLine();
+Console.WriteLine($"Нечетных чисел в массиве {negative}, четных числе {possitive}");
