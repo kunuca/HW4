c80020c1914be990fb30c5c13ce1c2f5dc050a6a
Tatiana Kustova <kunuca@mail.ru>
2023-02-13 14:25:48 +0400

    001

---
+++ 001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int[] numbers = new int[8];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]= new Random().Next(0,2);
+    Console.Write($" {numbers[i]} \t");
+}
